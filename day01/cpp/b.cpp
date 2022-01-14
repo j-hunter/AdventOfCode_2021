5a62085240dfee4cbfe1973119d90a9424dd74a4
Jonathan Hunter <j-hunter@users.noreply.github.com>
2022-01-14 08:56:26 +0000

    Finished day1 c++

--- day01/cpp/b.cpp
+++ day01/cpp/b.cpp
@@ -10,9 +10,13 @@ using namespace std;
 int main()
 {
 	string fname = "../input.txt";
-	int last = 0;
+	int last[3] = {0,0,0};
+	int lastTot = 0;
+	int idx = 0;
 	int next = 0;
+	int nextTot = 0;
 	int total = 0;
+	
 	bool first = true;
 
 	ifstream input;
@@ -25,14 +29,25 @@ int main()
 		next =  atoi(temp.c_str());
 		if (first)
 		{
-			first = false;
+			if(idx == 2)
+				first = false;
 		}
 		else
 		{
-			if (next > last)
+			nextTot = 0;
+		
+			for (int i = 0; i < 3; i++)
+				nextTot += last[i];
+
+
+			if (nextTot > lastTot)
 				total++;
+			lastTot = nextTot;
 		}
-		last = next;
+		last[idx] = next;
+
+		idx = (idx + 1) % 3;
+
 
 		
 	}
